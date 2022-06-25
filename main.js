2c1f42f2cabfb5d2b81fae01ae60373fb4c99687
anwesha13 <98837479+anwesha13@users.noreply.github.com>
2022-06-25 10:27:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var firstname="ranbir";
+console.log(firstname);
+var lastname="kapoor";
+console.log(lastname);
+
+
+var a="10";
+var b="5";
+
+var add=a+b;
+console.log(add);
+
+var multiply=a*b;
+console.log(multiply);
+ 
+var subtract=a-b;
+console.log(subtract);
+
+var divide=a/b;
+console.log(divide)
